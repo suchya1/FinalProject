b5a41461a7df5960c21d5f46d0235aad20ad7dd0
suchya1 <asuchy15@gmail.com>
2018-05-01 12:37:53 -0400

    Update to home page

--- server/ex/controller.js
+++ server/ex/controller.js
@@ -3,3 +3,21 @@ var Game = require('./model');
 
 var app = express.Router();
 
+var exercise=new exercise();
+
+module.exports = app
+    .get('/tips',(req,res)=>
+        res.send(exercise.GetTips(req.query.userId))
+    )
+    .post('/tips',(req,res)=>{
+        console.log(req.body);
+        try{
+            exercise.ChooseTip(req.body.Text,req.body.userId);
+            res.send({success:true});
+        }
+        catch(error){
+            res.status(403).send({success:false,message:error.message});
+        }
+    })
+    .get('/state',(req,res)=>res.send(exercise))
+    
